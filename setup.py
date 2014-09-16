77e7cb87ea4fef67300dea0fb1bd339515d68efa
Kenneth Giusti <kgiusti@redhat.com>
2014-09-16 14:00:41 -0400

    Release 1.1.1: add accessors for remote settle modes

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 from setuptools import setup
 
 setup(name="pyngus",
-      version="1.1.0",
+      version="1.1.1",
       author="kgiusti",
       author_email="kgiusti@apache.org",
       packages=["pyngus"],
