f2af498dbcd4865d7ce19e015f881278f27efec3
Kenneth Giusti <kgiusti@redhat.com>
2014-10-28 09:44:24 -0400

    Remove cut and paste code duplication

--- python/pyngus/link.py
+++ python/pyngus/link.py
@@ -305,21 +305,6 @@ def _get_remote_settle_modes(pn_link):
     return modes
 
 
-def _get_remote_settle_modes(pn_link):
-    """Return a map containing the settle modes as provided by the remote.
-    Skip any default value.
-    """
-    modes = {}
-    snd = pn_link.remote_snd_settle_mode
-    if snd == proton.Link.SND_UNSETTLED:
-        modes['snd-settle-mode'] = 'unsettled'
-    elif snd == proton.Link.SND_SETTLED:
-        modes['snd-settle-mode'] = 'settled'
-    if pn_link.remote_rcv_settle_mode == proton.Link.RCV_SECOND:
-        modes['rcv-settle-mode'] = 'second'
-    return modes
-
-
 class SenderEventHandler(object):
     def sender_active(self, sender_link):
         LOG.debug("sender_active (ignored)")
