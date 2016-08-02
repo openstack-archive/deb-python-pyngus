87a70cb950ac1986b4c18a58e6c87866b4908a8d
Kenneth Giusti <kgiusti@redhat.com>
2016-08-02 13:47:20 -0400

    v2.0.4

--- pyngus/__init__.py
+++ pyngus/__init__.py
@@ -23,4 +23,4 @@ from pyngus.link import SenderLink, SenderEventHandler
 from pyngus.sockets import read_socket_input
 from pyngus.sockets import write_socket_output
 
-VERSION = (2, 0, 3)  # major, minor, fix
+VERSION = (2, 0, 4)  # major, minor, fix
